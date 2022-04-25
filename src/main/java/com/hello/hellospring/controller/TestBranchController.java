39412ff0074ab640f6fcba28069029c978762602
jungseok_heo <tjrwjdgj@gmail.com>
2022-04-25 17:05:54 +0900

    test commit 브랜치

---
+++ src/main/java/com/hello/hellospring/controller/TestBranchController.java
@@ -0,0 +1,9 @@
+package com.hello.hellospring.controller;
+
+public class TestBranchController {
+
+    public static void main(String[] args) {
+        System.out.println("tset controller");
+        System.out.println("commit");
+    }
+}
